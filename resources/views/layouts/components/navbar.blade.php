ad9f898ca43a1084fefd0280de95e1f75871e41e
Oscar-CR <krz.oficial@gmail.com>
2024-04-08 12:33:29 -0600

    fix header

--- resources/views/layouts/components/navbar.blade.php
+++ resources/views/layouts/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <div  class="fixed top-0 left-0 right-0 z-50">
     
-    <nav class="w-full flex justify-between py-2 px-4 md:px-12 items-center flex-wrap bg-white shadow-md mb-4 pb-5" >
-        <div class="w-full md:w-1/12 mb-2 md:mb-0 mr-20">
+    <nav class="w-full flex justify-between py-2 px-4 md:px-12 items-center flex-wrap bg-white shadow-md mb-4 " style="padding-bottom:20px;">
+        <div class="w-full md:w-1/12 mb-2 md:mb-0 " style="margin-right: 100px;">
             <a href="{{ route('index') }}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="184.527" height="50" viewBox="0 0 184.527 76.5">
                     <defs>
